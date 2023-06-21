05c225bcafd70f7c1aee5d4e744b58469820b187
SG00819460 <134994110+SG00819460@users.noreply.github.com>
2023-06-21 11:01:15 +0000

    Update AppTest.java

--- src/test/java/com/javatechie/StringPalindrome/AppTest.java
+++ src/test/java/com/javatechie/StringPalindrome/AppTest.java
@@ -31,5 +31,9 @@ public class AppTest {
 	public void isNotPlaindromeExceptionTest() {
 		assertEquals(false, app.isPalindrome(null));
 	}
+	 @Test
+         public void aTest() {
+             assertEquals(true, true);
+        }
 	
 }
