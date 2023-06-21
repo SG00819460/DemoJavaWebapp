a0215ce987e24b8227e20b540b0fd97e39d298cd
SG00819460 <134994110+SG00819460@users.noreply.github.com>
2023-06-21 05:38:14 +0000

    Update AppTest.java

--- src/test/java/com/javatechie/StringPalindrome/AppTest.java
+++ src/test/java/com/javatechie/StringPalindrome/AppTest.java
@@ -1,18 +1,41 @@
+
 package com.javatechie.StringPalindrome;
 
 import static org.junit.Assert.assertEquals;
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-
+         private Palindrome palindrome;
+         private String input;
+	
         String input1 = "noon";
 	App app = new App();
 	boolean expected = true;
 
+    @Before
+    public void setUp() throws Exception {
+
+        input = null;
+        palindrome = new Palindrome();
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void nullStringTest() throws Exception {
+
+        palindrome.isPalindrome(null);
+
+    }
 	@Test
 	public void isPlaindromeTest() {
 		assertEquals(expected, app.isPalindrome(input1));
